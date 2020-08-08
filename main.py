4d4ad530f682f6d6ea84add916d4cadd8c4a88de
Akbar <akbaruddin@live.in>
2020-08-08 21:00:40 +0530

    user data

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from google.oauth2 import id_token
-from google.auth.transport import requests
+from google.auth.transport import requests as grequests
+import requests
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -39,5 +40,13 @@ class Item(BaseModel):
 def read_google_login(item: Item):
     getData = item.dict()
     # print(getData.get('clientId'))
-    idinfo = id_token.verify_oauth2_token(getData.get('credential'), requests.Request(), os.getenv("GOOGLE_CLIENT_ID"))
+    idinfo = id_token.verify_oauth2_token(getData.get('credential'), grequests.Request(), os.getenv("GOOGLE_CLIENT_ID"))
     return idinfo
+
+@app.post("/one-tap-login-basic")
+def read_google_login(item: Item):
+    getData = item.dict()
+    # print(getData.get('clientId'))
+    url = 'https://oauth2.googleapis.com/tokeninfo?id_token='+ getData.get('credential')
+    response = requests.get(url)
+    return response.json()
